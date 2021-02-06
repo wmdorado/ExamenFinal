ecd0512aea09f93577c16e06d9e73ab5fdb47282
wmdorado <a2017210277@estudiantes.upsa.edu.bo>
2021-02-06 03:14:52 +0100

    UnitTest Get

---
+++ SolNumeroLiteralWM/NUnitTestNumeroLiteral/NumeroLiteralController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NUnitTestNumeroLiteral
+{
+    internal class NumeroLiteralController
+    {
+        internal string ConversionesGet(string num1, string num2, string num3, string num4, string num5)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string ConversionesPost(string num1, string num2, string num3, string num4, string num5)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SolNumeroLiteralWM/NUnitTestNumeroLiteral/UnitTest1.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+
+namespace NUnitTestNumeroLiteral
+{
+    public class TestNumeroLiteal
+    {
+        public object NumeroLiteralController { get; private set; }
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestConversionesGet()
+        {
+            //Arrange=preparacion
+
+            string num1 = "8";
+            string num2 = "5";
+            string num3 = "10.2";
+            string num4 = "-3";
+            string num5 = "1234567.89";
+            string esperado1 = "CERO";
+            string esperado2 = "CINCO";
+            string esperado3 = "DIEZ PUNTO DOS";
+            string esperado4 = "MENOS TRES";
+            string esperado5 = "UN MILLON DOCIENTOS TREINTA Y CUATRO MIL QUINIENTOS SESENTA Y SIETE PUTO OCENTA Y NUEVE";
+
+            NumeroLiteralController conv = new NumeroLiteralController();
+
+            //Act=ejecucion
+
+            string resultado = conv.ConversionesGet(num1, num2, num3, num4, num5);
+
+            //Assert=verificacion
+
+            Assert.AreEqual(esperado5, resultado);
+        }
+       
+    }
+}
